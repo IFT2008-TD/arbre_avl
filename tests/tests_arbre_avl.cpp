1f92c99c7fb6bd792a8fea37224104934dfe1ce4
Pascal Charpentier <charpentier_pasc@hotmail.com>
2022-07-01 13:31:54 -0400

    Clé, valeur, optimisation de aux_retirer_noeud, invariant complété.

--- tests/tests_arbre_avl.cpp
+++ tests/tests_arbre_avl.cpp
@@ -7,20 +7,20 @@
 
 class Arbre_AVL_Test : public ::testing::Test {
 protected:
-    Arbre_AVL<size_t> arbre_vide ;
-    Arbre_AVL<size_t> arbre_1 ;
-    Arbre_AVL<size_t> arbre_2 ;
-    Arbre_AVL<size_t> arbre_3 ;
+    Arbre_AVL<size_t, size_t> arbre_vide ;
+    Arbre_AVL<size_t, size_t> arbre_1 ;
+    Arbre_AVL<size_t, size_t> arbre_2 ;
+    Arbre_AVL<size_t, size_t> arbre_3 ;
 
     void SetUp () override {
-        arbre_1.inserer(1) ;
+        arbre_1.inserer(1, 1000) ;
 
-        arbre_2.inserer(1) ;
-        arbre_2.inserer(2) ;
+        arbre_2.inserer(1, 1000) ;
+        arbre_2.inserer(2, 2000) ;
 
-        arbre_3.inserer(2) ;
-        arbre_3.inserer(1) ;
-        arbre_3.inserer(3) ;
+        arbre_3.inserer(2, 2000) ;
+        arbre_3.inserer(1, 1000) ;
+        arbre_3.inserer(3, 3000) ;
     }
 };
 
@@ -49,7 +49,7 @@ TEST_F(Arbre_AVL_Test, arbre_un_element_cardinal_egal_1) {
 }
 
 TEST_F(Arbre_AVL_Test, arbre_un_element_rechercher_cle_presente) {
-    EXPECT_EQ(1, arbre_1.rechercher(1)) ;
+    EXPECT_EQ(1000, arbre_1.rechercher(1)) ;
 }
 
 TEST_F(Arbre_AVL_Test, arbre_un_element_rechercher_cle_absente_throw) {
@@ -89,8 +89,8 @@ TEST_F(Arbre_AVL_Test, arbre_deux_elements_cardinal_2) {
 }
 
 TEST_F(Arbre_AVL_Test, arbre_deux_elements_cles_1_2_presentes) {
-    EXPECT_EQ(1, arbre_2.rechercher(1)) ;
-    EXPECT_EQ(2, arbre_2.rechercher(2)) ;
+    EXPECT_EQ(1000, arbre_2.rechercher(1)) ;
+    EXPECT_EQ(2000, arbre_2.rechercher(2)) ;
 }
 
 TEST_F(Arbre_AVL_Test, arbre_deux_elements_cle_absente_throw) {
@@ -109,7 +109,7 @@ TEST_F(Arbre_AVL_Test, arbre_deux_elements_cardinal_1_apres_suppression) {
 
 TEST_F(Arbre_AVL_Test, arbre_deux_elements_cle_restante_apres_suppression) {
     arbre_2.supprimer(1) ;
-    EXPECT_EQ(2, arbre_2.rechercher(2)) ;
+    EXPECT_EQ(2000, arbre_2.rechercher(2)) ;
 }
 
 TEST_F(Arbre_AVL_Test, arbre_deux_elements_cle_2_absente_apres_suppression) {
@@ -124,7 +124,7 @@ TEST_F(Arbre_AVL_Test, arbre_deux_elements_cardinal_1_apres_suppression_cle_2) {
 
 TEST_F(Arbre_AVL_Test, arbre_deux_elements_cle_1_restante_apres_suppression) {
     arbre_2.supprimer(2) ;
-    EXPECT_EQ(1, arbre_2.rechercher(1)) ;
+    EXPECT_EQ(1000, arbre_2.rechercher(1)) ;
 }
 
 TEST_F(Arbre_AVL_Test, arbre_deux_elements_vide_apres_2_suppressions) {
@@ -140,8 +140,8 @@ TEST_F(Arbre_AVL_Test, arbre_trois_elements_cardinal_2_apres_suppression) {
     arbre_3.supprimer(2) ;
     EXPECT_FALSE(arbre_3.vide()) ;
     EXPECT_EQ(2, arbre_3.cardinal()) ;
-    EXPECT_EQ(1, arbre_3.rechercher(1)) ;
-    EXPECT_EQ(3, arbre_3.rechercher(3)) ;
+    EXPECT_EQ(1000, arbre_3.rechercher(1)) ;
+    EXPECT_EQ(3000, arbre_3.rechercher(3)) ;
     EXPECT_THROW(arbre_3.rechercher(2), std::runtime_error) ;
 }
 
@@ -150,7 +150,7 @@ TEST_F(Arbre_AVL_Test, arbre_trois_elements_cardinal_1_apres_2_suppressions) {
     arbre_3.supprimer(1) ;
     EXPECT_FALSE(arbre_3.vide()) ;
     EXPECT_EQ(1, arbre_3.cardinal()) ;
-    EXPECT_EQ(3, arbre_3.rechercher(3)) ;
+    EXPECT_EQ(3000, arbre_3.rechercher(3)) ;
     EXPECT_THROW(arbre_3.rechercher(1), std::runtime_error) ;
     EXPECT_THROW(arbre_3.rechercher(2), std::runtime_error) ;
 }
